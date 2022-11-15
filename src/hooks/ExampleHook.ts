cc971d9c87c18ce72fdb93ba9593e33d3ddac4b7
Kallum Parr <kallum.parr@fanduel.com>
2022-11-15 15:49:13 +0000

    Update text.

--- src/hooks/ExampleHook.ts
+++ src/hooks/ExampleHook.ts
@@ -1,13 +1,14 @@
 import React from 'react';
 
 /*
-An example hook that consumes stable state from Redux but contains an unstable function reference.
-The useEffect is unintentionally invoked due to the parent's cascading render causing this hook to re-evaluate causing a new function
-to be created in useTriggerAnalytics which is included in the useEffect deps.
+    An example hook that consumes stable state from Redux but contains an unstable function reference.
+    
+    The useEffect is unintentionally invoked due to the parent's cascading render causing this hook to re-evaluate and a new function
+    to be created and returned from useTriggerAnalytics which is included in the useEffect deps.
  */
 export default function useExampleHook() {
     const triggerAnalytics = useTriggerAnalytics();
-    
+
     // const firstStableExample = useSelector(state => state.first);
     // const secondStableExample = useSelector(state => state.second);
 
@@ -21,7 +22,7 @@ export default function useExampleHook() {
     ])
 }
 
-function useTriggerAnalytics(){
+function useTriggerAnalytics() {
     return () => {
         console.log(`Example analytics`)
     }
